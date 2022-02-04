50574fb5ae60a23f35754d6be9c23ce849688709
Roger9 <gutierrez.rog12@gmail.com>
2022-02-04 11:02:07 -0300

    Server config

--- public/realtime-chat/main.js
+++ public/realtime-chat/main.js
@@ -1,2 +1,13 @@
 // eslint-disable-next-line no-undef
 const socket = io()
+const $formChat = document.getElementById('formChat')
+
+socket.on('message', message => {
+  console.log(message)
+})
+
+$formChat.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const msg = e.target.messageChat.value
+  socket.emit('chatMessage', msg)
+})
--- src/server.js
+++ src/server.js
@@ -11,12 +11,14 @@ app.use(logger)
 const server = createServer(app)
 const io = socketio(server)
 
-io.on('connection', socket => console.log('New WS connection...'))
+io.on('connection', socket => {
+  socket.emit('message', 'Welcome to Chat!')
 
-// const PORT = process.env.PORT ?? 4000
-// ;(async () => {
-//   await server.listen(PORT)
-//   console.log('serve on PORT', PORT)
-// })()
+  socket.broadcast.emit('message', 'A user has joined the chat!')
+
+  socket.on('disconnect', () => io.emit('message', 'A user has left the chat!'))
+
+  socket.on('chatMessage', message => io.emit('message', message))
+})
 
 module.exports = server
